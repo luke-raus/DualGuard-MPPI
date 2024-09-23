a5faf8a9e2ad9b986768c9efa55830931e12a7d5
Luke <luke@lukera.us>
2024-09-23 13:08:36 -0700

    Fix analysis post-refactor

--- MPPI-with-reachability/analyze_experiments.py
+++ MPPI-with-reachability/analyze_experiments.py
@@ -2,7 +2,7 @@ from omegaconf import OmegaConf
 from pathlib import Path
 import pandas as pd
 
-from experiment_result import ExperimentResult
+from experiment_storage import ExperimentStorage
 
 
 experiments_path = Path('experiments')
@@ -12,23 +12,27 @@ control_profiles_fname = Path('config') / 'control_profiles.yaml'
 
 control_profiles = OmegaConf.load(control_profiles_fname)
 
-experiments_info = [ExperimentResult(x).get_all_experiment_info() for x in sorted(experiments_path.iterdir())]
-exp_df = pd.DataFrame(experiments_info)
-print(exp_df.keys())
+experiments_info = [ExperimentStorage(x).get_all_experiment_info() for x in sorted(experiments_path.iterdir())]
+all_exp_df = pd.DataFrame(experiments_info)
+print(all_exp_df.keys())
 
-num_samples_settings = exp_df['mppi_samples'].unique().tolist()
+num_samples_settings = all_exp_df['mppi_samples'].unique().tolist()
 
-config_results = []
 for num_samples in num_samples_settings:
+
+    config_results = []
+
     for profile_name, settings in control_profiles.items():
 
-        config_trials = exp_df.loc[ (exp_df['control_profile'] == profile_name) ]
+        config_trials = all_exp_df.loc[   (all_exp_df['control_profile'] == profile_name) 
+                                        & (all_exp_df['mppi_samples'] == num_samples) ]
         # Alternatively, filter for all relevant settings using filter_df_by_dict()
 
         num_trials = len(config_trials)
 
         results = {
             'profile name': profile_name,
+            'mppi samples': num_samples,
             'num trials':  num_trials,
             'cost (avg ± std)': f"{config_trials['total_cost'].mean():.1f} ± {config_trials['total_cost'].std():.1f}",
             'crashed (%)':      100 * config_trials['crashed'].sum() / num_trials,
