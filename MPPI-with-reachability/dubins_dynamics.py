a79e4a2f22cec581a98af9511d69c39456c51b00
Luke <luke@lukera.us>
2025-02-19 14:17:47 -0800

    Clean up dynamics class

--- MPPI-with-reachability/dubins_dynamics.py
+++ MPPI-with-reachability/dubins_dynamics.py
@@ -3,22 +3,14 @@ import numpy as np
 
 class DubinsCarFixedVel():
     """
-    Using the 3-state, 2-input Dubins dynamics described in section VI-B of
-    Gandhi et al 2023 on Safe Importance Sampling in MPPI
+    Implements 3-state, 1-input (steering) dynamics of fixed-speed Dubins Car
     """
 
     def __init__(self, timestep, linvel, init_state):
-        """
-        Initializes a class of the SimpleDubins vehicle, with a 'nominal' motion model
-        """
-
         self.nx = 3
         self.nu = 1
-
         self.linvel = linvel
-
         self.state = init_state
-
         self.timestep = timestep
 
     def get_state(self):
@@ -29,27 +21,11 @@ class DubinsCarFixedVel():
         self.state[1] = y
         self.state[2] = theta
 
-
     def _next_states_batch(self, states, controls):
         """
-        #TODO: Reference for exact Dubins integration
+        Performs exact forward-integration of Dubins dynamics
 
         states (K, nx=3), controls (K, nu=1) -> next_states (K, nx=3)
-
-        For duration of timestep, system travels in arc of curvature/radius:
-            K = angvel/linvel
-            r = linvel/angvel
-        Where amount turned is difference in heading:
-            d_theta = angvel*dt
-        Center point C=(C_x;C_y) of arc based on current state (x;y;heading) & radius:
-            C_x = S_x - r*sin(theta)
-            C_y = S_y + r*cos(theta)
-        Now we get the new state x;y vector S' by this procedure:
-            Get vector P of length r from rotation center C to state S (S=C+P -> P=S-C)
-            Rotate P by d_theta rotation matrix to get P'
-            Add P' back to C to get S' (S'=C+P')
-        Expanding P', this looks like:
-            S' = C + R_{d_theta}@(S-C)
         """
         x = states[:, 0]
         y = states[:, 1]
@@ -58,9 +34,7 @@ class DubinsCarFixedVel():
 
         # Get indices of zero-inputs for regular straight-line integration
         no_steer = (angvel == 0.0)
-
-        # FIXME: This dubins integration seems overly complex for a fixed-velocity model we should use the usual form which should be faster an has no weird edge cases   
-        # check discrete_dynamics() in experiment_runner.py for a simpler version     
+  
         # Compute turning radius -> steering center -> new state x/y as above
         # This won't fail if omega=0: will just produce NaN/Inf's
         d_theta = self.timestep * angvel
@@ -81,12 +55,11 @@ class DubinsCarFixedVel():
         # Stack along dimension 1 to go from 3 vectors of K values to K (state) vectors of 3 values
         return np.column_stack((x_next, y_next, theta_next))
 
-
     def next_states(self, state, control):
         """
         state, control -> next_state
-        Option single state   - inputs are dim=1 tensors: state (nx=3), control (nu=1)
-        Option multiple sttae - inputs are dim=2 tensors: state (K, nx=3), control (K, nu=1)
+        Option single state   - inputs are dim=1 arrays: state (nx=3), control (nu=1)
+        Option multiple sttae - inputs are dim=2 arrays: state (K, nx=3), control (K, nu=1)
         """
         if state.ndim == 1:
             # Singleton state input; unsqueeze to pass to batch function
@@ -96,12 +69,8 @@ class DubinsCarFixedVel():
             assert (control.ndim > 1)
             return self._next_states_batch(state, control)
 
-    def update_true_state(self, control):
+    def update_true_state(self, control) -> None:
         """
         Apply single control input to update state of system
-        Input:
-            control is (nu)
-        Output:
-            None; just updates current true state (self.state)
         """
         self.state = self.next_states(self.state, control)
